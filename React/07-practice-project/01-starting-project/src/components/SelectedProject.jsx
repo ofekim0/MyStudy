36a05dc734d5e640f973ae530124798bd41f8e2b
이정준 <ofekim0@naver.com>
2024-07-05 21:09:38 +0900

    24-07-05

--- React/07-practice-project/01-starting-project/src/components/SelectedProject.jsx
+++ React/07-practice-project/01-starting-project/src/components/SelectedProject.jsx
@@ -1,4 +1,12 @@
-export default function SelectedProject({ project, onDelete }) {
+import Tasks from "./Tasks";
+
+export default function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -23,6 +31,7 @@ export default function SelectedProject({ project, onDelete }) {
           {project.description}
         </p>
       </header>
+      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
     </div>
   );
 }
