e14a7cb2289d373d0e93291fd96c475cc9cca7c2
이정준 <ofekim0@naver.com>
2024-07-17 23:24:14 +0900

    240717

--- React/15-adv-starting-project/12-adv-starting-project/frontend/src/pages/EditEvent.js
+++ React/15-adv-starting-project/12-adv-starting-project/frontend/src/pages/EditEvent.js
@@ -1,7 +1,11 @@
+import { useRouteLoaderData } from 'react-router-dom';
 import EventForm from '../components/EventForm'
 
 function EditEventPage() {
-  return <EventForm />;
+  const data = useRouteLoaderData('event-detail');
+  const event = data.event;
+
+  return <EventForm event={event} />;
 }
 
 export default EditEventPage;
--- React/15-adv-starting-project/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ React/15-adv-starting-project/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,23 +1,38 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useLoaderData()
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <EventItem event={data.event} />
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
-export async function loader({request, params}) {
-  const id = params.eventId
-  const response = await fetch('http://localhost:8080/events/' + id)
-  
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
   if (!response.ok) {
-    throw json({message: 'Could not fetch details for selected event'}, { status: 500})
+    throw json(
+      { message: "Could not fetch details for selected event" },
+      { status: 500 }
+    );
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+  if (!response.ok) {
+    throw json(
+      { message: "Could not delete event." },
+      { status: 500 }
+    );
+  }
+  return redirect('/events')
 }
