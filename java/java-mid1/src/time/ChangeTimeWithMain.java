70016edd44e51ece908196cd38e6664d0202cbbe
이정준 <ofekim0@naver.com>
2024-08-25 20:57:39 +0900

    08-25

---
+++ java/java-mid1/src/time/ChangeTimeWithMain.java
@@ -0,0 +1,19 @@
+package time;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+
+public class ChangeTimeWithMain {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.of(2018, 1, 1, 13, 30, 59);
+        System.out.println("dt = " + dt);
+
+        LocalDateTime changedDt1 = dt.with(ChronoField.YEAR, 2020);
+        System.out.println("changedDt1 = " + changedDt1);
+
+        LocalDateTime changedDt2 = dt.withYear(2020);
+        System.out.println("changedDt2 = " + changedDt2);
+
+
+    }
+}
